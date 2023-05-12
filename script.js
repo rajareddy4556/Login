79c2867655ab4f09445701658fa3aa1f77cc277c
Chinthala Rajasekhara Reddy <68636215+rajareddy4556@users.noreply.github.com>
2023-05-12 12:26:55 +0530

    yes

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
 document.getElementById("hello").style.display="none";
 let c=0;
-let i=document.getElementById("b1");
+let i=document.getElementById("b3");
 i.click();
+window.onload=function(){
+  var myDiv = document.getElementById("hel");
+  myDiv.style.display = "none"; 
+};
 function fun(){
   document.getElementById("hel").style.display="none";
   document.getElementById("b2").style.borderBottom="none";
+  document.getElementById("b3").style.borderBottom="none";
+
     document.getElementById("b1").style.borderBottom="3px solid Red"
-      c++;
-    if(c%2==1)
     document.getElementById("hello").style.display="block";
 }
 function fun1(){
+  document.getElementById("b3").style.borderBottom="none";
   document.getElementById("b1").style.borderBottom="none";
   document.getElementById("hello").style.display="none";
   document.getElementById("b2").style.borderBottom="3px solid Red"
-    c++;
-  if(c%2==0)
   document.getElementById("hel").style.display="block";
 }
+function fun2(){
+  let a=document.getElementById("e1").value;
+  let b=document.getElementById("p1").value;
+  if(a=='admin' && b=='admin'){
+    window.location="form.html";
+  }
+  else if(a=='' || b==''){
+    window.alert("plz enter all credentials");
+  }
+  else{
+    window.alert("wrong credentials");
+  }
+}
+function fun3(){
+     document.getElementById("hello").style.display="none";
+     document.getElementById("hel").style.display="none";
+    document.getElementById("b1").style.borderBottom="none";
+    document.getElementById("b2").style.borderBottom="none";
+    document.getElementById("b3").style.borderBottom="3px solid Red"
+}
+let sname=document.getElementById("n1").value;
+let branch=document.getElementById("n2").value;
+let dreamjob=document.getElementById("n3").value;
+let gender=document.getElementById("n4").value; 
